f5a0e9901855184f4638134f74b7cc803ac3abdf
Danyal Raza <smdraza@uwaterloo.ca>
2017-02-04 02:28:26 -0500

    add blur

--- app.py
+++ app.py
@@ -9,7 +9,6 @@ from helper import getMoleContour
 from helper import showImage
 import pandas as pd
 from skimage import io
-from learn import AnalysisSpec
 from sklearn import cross_validation
 from sklearn.cluster import MiniBatchKMeans
 from sklearn.ensemble import RandomForestClassifier
@@ -20,9 +19,13 @@ from sklearn import svm
 
 def find_mole(origimage):
     image = cv2.imread(origimage)
+    image_blur = cv2.blur(image, (5, 5))
+    image_greyscale = cv2.cvtColor(image_blur, cv2.COLOR_BGR2GRAY)
+    image_contour = getMoleContour(image_greyscale)
     max_dimension = max(image.shape)
     scale = 700/max_dimension
     image = cv2.resize(image, None, fx=scale, fy=scale)
-    showimage(image)
+
+    showImage(image_contour)
 
 find_mole('./img/malignant.jpg')
