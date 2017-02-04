616acc69a9c53949d80f831b24d6a57e49fe79a2
Alston Lin <alstonlin812@gmail.com>
2017-02-04 01:50:29 -0500

    Added helper countour and showImage functions

--- cv/calc_jagedness.py
+++ cv/calc_jagedness.py
@@ -1,11 +1,13 @@
 import cv2
+import helper
 
-def calculateJagedness(image):
-    cv2.imshow("image", img)
-    cv2.waitKey(0)
-    cv2.destroyAllWindows()
+def calculateJagedness(image, debug=False):
+    # Contours    
+    contour = helper.getMoleContour(image)
+    if debug:
+        cv2.drawContours(image, contour, -1, (255, 255, 255), 5)
+        helper.showImage(image)
 
 if __name__ == "__main__":
-    img = cv2.imread("./img/malignant.jpg", 0)
-    calculateJagedness(img)
-
+    grayscale = cv2.imread("./img/malignant.jpg", 0)
+    calculateJagedness(grayscale, debug=True)
---
+++ cv/helper.py
@@ -0,0 +1,21 @@
+import cv2
+
+def getMoleContour(image):
+    ret, thresh = cv2.threshold(image, 150, 200, cv2.THRESH_BINARY_INV) # Binary mask
+    showImage(thresh)
+    im2, contours, hierarchy = cv2.findContours(thresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE) 
+    # Check to make sure there actually is a contour
+    if len(contours) == 0:
+        raise Error("No contours found!")
+    # Only find the longest one
+    longestContour = []
+    for i in range(len(contours)):
+        if len(contours[i]) > len(longestContour):
+            longestContour = contours[i]
+    return longestContour
+
+def showImage(image):
+    cv2.imshow("image", image)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+
