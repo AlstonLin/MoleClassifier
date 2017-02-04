a5026feacc995bb6e754bdf77b40b6e69fc0aa06
Danyal Raza <smdraza@uwaterloo.ca>
2017-02-04 02:17:18 -0500

    add initial stuff

---
+++ app.py
@@ -0,0 +1,34 @@
+
+
+import border
+import color
+import pickle
+import cv2
+import numpy as np
+import os
+import assymmetry
+import sys
+sys.path.append('./cv/')
+import standardize
+import calc_jagedness
+from helper import getMoleContour
+from helper import showImage
+import pandas as pd
+from skimage import io
+from learn import AnalysisSpec
+from sklearn import cross_validation
+from sklearn.cluster import MiniBatchKMeans
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import precision_score
+from sklearn.externals import joblib
+from sklearn.naive_bayes import GaussianNB, MultinomialNB
+from sklearn import svm
+
+def find_mole(origimage):
+    image = cv2.imread(origimage)
+    max_dimension = max(image.shape)
+    scale = 700/max_dimension
+    image = cv2.resize(image, None, fx=scale, fy=scale)
+    showimage(image)
+
+find_mole('./img/malignant.jpg')
--- cv/helper.py
+++ cv/helper.py
@@ -3,7 +3,7 @@ import cv2
 def getMoleContour(image):
     ret, thresh = cv2.threshold(image, 150, 200, cv2.THRESH_BINARY_INV) # Binary mask
     showImage(thresh)
-    im2, contours, hierarchy = cv2.findContours(thresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE) 
+    im2, contours, hierarchy = cv2.findContours(thresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
     # Check to make sure there actually is a contour
     if len(contours) == 0:
         raise Error("No contours found!")
@@ -18,4 +18,3 @@ def showImage(image):
     cv2.imshow("image", image)
     cv2.waitKey(0)
     cv2.destroyAllWindows()
-
