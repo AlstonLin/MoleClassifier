d6d1570c82023f8f77c9a97008ec85e8393a385a
Alston Lin <alstonlin812@gmail.com>
2017-02-04 03:11:55 -0500

    Finished jaggedness calculation

--- cv/calc_jagedness.py
+++ cv/calc_jagedness.py
@@ -1,13 +1,37 @@
 import cv2
+import math
 import helper
 
 def calculateJagedness(image, debug=False):
     # Contours    
     contour = helper.getMoleContour(image)
+    # Fits an ellipse around the image
+    ellipse = cv2.fitEllipse(contour)
+    # Shows the contour and ellipse for debugging
     if debug:
         cv2.drawContours(image, contour, -1, (255, 255, 255), 5)
+        cv2.ellipse(image, ellipse, (0, 0, 0), 5)
         helper.showImage(image)
+    # Calculates the mean square distance between the ellipse and contour
+    center = ellipse[0]
+    dim = ellipse[1]
+    theta = ellipse[2]
+    meanSquare = 0
+    for i in range(1, len(contour)):
+        diffX = (contour[i][0][0] - center[0]) * math.cos(-theta) - \
+                (contour[i][0][1] - center[1]) * math.sin(-theta)
+        diffY = (contour[i][0][0] - center[0]) * math.sin(-theta) - \
+                (contour[i][0][1] - center[1]) * math.cos(-theta)
+        meanSquare += abs((diffX / dim[0]) ** 2 + (diffY / dim[1]) ** 2)
+    return meanSquare
 
 if __name__ == "__main__":
-    grayscale = cv2.imread("./img/malignant.jpg", 0)
-    calculateJagedness(grayscale, debug=True)
+    # Tests
+    grayscale = cv2.imread("./img/malignant1.jpg", 0)
+    print(calculateJagedness(grayscale, debug=True))
+    grayscale = cv2.imread("./img/malignant2.jpg", 0)
+    print(calculateJagedness(grayscale, debug=True))
+    grayscale = cv2.imread("./img/benign1.jpg", 0)
+    print(calculateJagedness(grayscale, debug=True))
+    grayscale = cv2.imread("./img/benign2.jpg", 0)
+    print(calculateJagedness(grayscale, debug=True))
--- cv/helper.py
+++ cv/helper.py
@@ -1,7 +1,10 @@
 import cv2
 
-def getMoleContour(image):
-    ret, thresh = cv2.threshold(image, 150, 200, cv2.THRESH_BINARY_INV) # Binary mask
+# NOTE: The given image must be a greyscale image
+def getMoleContour(greyscale):
+    THRESHOLD = 125
+    MAX_VALUE = 180
+    ret, thresh = cv2.threshold(greyscale, THRESHOLD, MAX_VALUE, cv2.THRESH_BINARY_INV) # Binary mask
     showImage(thresh)
     im2, contours, hierarchy = cv2.findContours(thresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE) 
     # Check to make sure there actually is a contour
